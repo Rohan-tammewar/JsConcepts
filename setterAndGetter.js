d82cbeda76dde34d0daf9d787b887a2f5f164e94
Rohan-tammewar <rohan.tammewar@gmail.com>
2023-04-21 13:21:16 +0530

    setter and getter with cache

--- setterAndGetter.js
+++ setterAndGetter.js
@@ -1,15 +1,15 @@
 //setter and getter
 //The get and set syntax binds an object property to a function that will be called when that property is looked up. It can also be used in classes.
 // When you write a set function it acts as a property
-const obj = {
-  log: [],
-  set setLatest(n) {
-    this.log.push(n)
-  },
-  get getLatest() {
-    return [...this.log]
-  },
-}
+// const obj = {
+//   log: [],
+//   set setLatest(n) {
+//     this.log.push(n)
+//   },
+//   get getLatest() {
+//     return [...this.log]
+//   },
+// }
 //obj.setLatest = 1
 //log =[1]
 //   console.log(obj.getLatest);
@@ -26,3 +26,43 @@ set(key, value, duration): accepts an integer key, an integer value, and a durat
 get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
 
 count(): returns the count of un-expired keys. */
+
+//Constructor since, the first letter is in UpperCase
+const TimeLimitedCache = function () {
+  this.keys = new Map()
+}
+
+//Writing a setter and getter function in the objects prototype so, it can be accessed by all instance of the above object
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  const isKeyExist = this.keys.has(key)
+
+  if (isKeyExist) {
+    let [, oldExpiry] = this.keys.get(key)
+    clearTimeout(oldExpiry) // cancels the previous timer set using setTimeout
+  }
+
+  const expiry = setTimeout(() => {
+    this.keys.delete(key)
+  }, duration)
+
+  this.keys.set(key, [value, expiry])
+
+  return isKeyExist
+}
+
+TimeLimitedCache.prototype.get = function (key) {
+  return this.keys.has(key) ? this.keys.get(key)[0] : -1
+}
+
+TimeLimitedCache.prototype.count = function () {
+  return this.keys.size
+}
+
+const obj = new TimeLimitedCache()
+console.log(obj.set(1, 42, 5000) + ', ' + obj.get(1) + ', ' + obj.count())
+setTimeout(() => {
+  console.log(obj.get(1))
+}, 2000) // key exists op: 42
+setTimeout(() => {
+  console.log(obj.get(1))
+}, 6000) // op:-1
